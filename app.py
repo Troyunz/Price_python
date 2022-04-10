637d67afb808a511c8c7d816a1a8ee6d67f99d16
Troyunz <titarun28@gmail.com>
2022-04-10 23:18:55 +0530

    updated stock, site, cpu, memory

--- app.py
+++ app.py
@@ -5,14 +5,47 @@ import requests
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///price.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///storage.db"
+app.config['SQLALCHEMY_BINDS'] = {"cpu":"sqlite:///cpu.db",
+                                  "memory": "sqlite:///memory.db"}
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-class Price(db.Model):
+class Storage(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
+    sku = db.Column(db.String(1000), nullable=False)
     products = db.Column(db.String(500), nullable=False)
     price = db.Column(db.Integer, nullable=False)
+    stock = db.Column(db.String(100), nullable=False)
+    site = db.Column(db.String(100), nullable=False)
+    url = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"{self.sno} - {self.price}"
+    
+class Processor(db.Model):
+    __bind_key__ = 'cpu'
+    sno = db.Column(db.Integer, primary_key=True)
+    sku = db.Column(db.String(1000), nullable=False)
+    products = db.Column(db.String(500), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    site = db.Column(db.String(100), nullable=False)
+    stock = db.Column(db.String(100), nullable=False)
+    url = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"{self.sno} - {self.price}"
+
+class Memory(db.Model):
+    __bind_key__ = 'memory'
+    sno = db.Column(db.Integer, primary_key=True)
+    sku = db.Column(db.String(1000), nullable=False)
+    products = db.Column(db.String(500), nullable=False)
+    site = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    stock = db.Column(db.String(100), nullable=False)
     url = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
@@ -49,23 +82,74 @@ def storage():
     # data = json.loads(req.content)
     # for i in range(len(data)):
     #     title =  data[i]['title']
+    #     sku = data[i]['sku']
+    #     stock = data[i]['stock']
     #     price = data[i]['price']
+    #     site = data[i]['site']
     #     price = price.replace('₹', '')
     #     price = price.replace(',', '')
-    #     price = int(price)
+    #     price = float(price)
     #     url = data[i]['url']
     #     # desc = request.form['desc']
-    #     prices = Price(products=title, price=price, url=url)
+    #     prices = Storage(products=title, price=price, url=url, sku=sku, stock=stock, site=site)
     #     db.session.add(prices)
     #     db.session.commit()
         
     # print(len(data))
-    allPrice = Price.query.all() 
+    allPrice = Storage.query.all() 
     return render_template('storage.html', data=allPrice)
+
+@app.route('/cpu')
+def cpu():
+    # if request.method=='POST':
+    # req = requests.get("https://scrapedcb-node-deploy.herokuapp.com/getprocessor")
+    # data = json.loads(req.content)
+    # for i in range(len(data)):
+    #     title =  data[i]['title']
+    #     sku = data[i]['sku']
+    #     stock = data[i]['stock']
+    #     site = data[i]['site']
+    #     price = data[i]['price']
+    #     price = price.replace('₹', '')
+    #     price = price.replace(',', '')
+    #     price = float(price)
+    #     url = data[i]['url']
+    #     # desc = request.form['desc']
+    #     prices = Processor(products=title, price=price, url=url, sku=sku, stock=stock, site=site)
+    #     db.session.add(prices)
+    #     db.session.commit()
+        
+    # print(len(data))
+    allPrice = Processor.query.all() 
+    return render_template('processor.html', data=allPrice)
+
+@app.route('/memory')
+def memory():
+    # if request.method=='POST':
+    # req = requests.get("https://scrapedcb-node-deploy.herokuapp.com/getmemory")
+    # data = json.loads(req.content)
+    # for i in range(len(data)):
+    #     title =  data[i]['title']
+    #     sku = data[i]['sku']
+    #     site = data[i]['site']
+    #     stock = data[i]['stock']
+    #     price = data[i]['price']
+    #     price = price.replace('₹', '')
+    #     price = price.replace(',', '')
+    #     price = float(price)
+    #     url = data[i]['url']
+    #     # desc = request.form['desc']
+    #     prices = Memory(products=title, price=price, url=url, sku=sku, stock=stock, site=site)
+    #     db.session.add(prices)
+    #     db.session.commit()
+        
+    # print(len(data))
+    allPrice = Memory.query.all() 
+    return render_template('memory.html', data=allPrice)
 
 @app.route('/show')
 def products():
-    allTodo = Price.query.all()
+    allTodo = Storage.query.all()
     print(allTodo)
     return 'this is products page'
 
@@ -74,22 +158,22 @@ def update(sno):
     if request.method=='POST':
         title = request.form['title']
         desc = request.form['desc']
-        price = Price.query.filter_by(sno=sno).first()
+        price = Storage.query.filter_by(sno=sno).first()
         price.title = title
         price.desc = desc
         db.session.add(price)
         db.session.commit()
         return redirect("/")
         
-    price = Price.query.filter_by(sno=sno).first()
+    price = Storage.query.filter_by(sno=sno).first()
     return render_template('update.html', price=price)
 
 @app.route('/delete/<int:sno>')
 def delete(sno):
-    price = Price.query.filter_by(sno=sno).first()
+    price = Storage.query.filter_by(sno=sno).first()
     db.session.delete(price)
     db.session.commit()
     return redirect("/")
 
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=True, port=8000)
--- static/script/script.js
+++ static/script/script.js
@@ -1,6 +1,13 @@
 $(document).ready(function () {
     $('#priceStorage').DataTable({
-        "pageLength": 5000,
+        "pageLength": 50,
         // searching: false 
+        "autoWidth": false, // might need this
+        "columns": [
+            { "width": "2%"}, // automatically calculates
+            { "width": "80%" },
+             null,  // remaining width
+             null
+  ]
     });
   });
