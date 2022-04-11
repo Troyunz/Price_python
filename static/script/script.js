31e2fd7ad0bb8f06c715b597399de9efe87b62c9
Troyunz <titarun28@gmail.com>
2022-04-11 23:02:49 +0530

    change 2

--- static/script/script.js
+++ static/script/script.js
@@ -3,11 +3,22 @@ $(document).ready(function () {
         "pageLength": 50,
         // searching: false 
         "autoWidth": false, // might need this
-        "columns": [
-            { "width": "2%"}, // automatically calculates
-            { "width": "80%" },
-             null,  // remaining width
-             null
-  ]
+        // "columns": [
+        //     { "width": "2%"}, // automatically calculates
+        //     { "width": "80%" },
+        //      null,  // remaining width
+        //      null
+        // ],
+        columns: [
+          null,
+          null,
+          {orderable: false, searchable: false},
+          {searchable: false},
+        ],
+        "language": {
+          "paginate": {
+            "next": "Next page"
+          }
+        }
     });
   });
